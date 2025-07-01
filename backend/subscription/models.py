85f704342ae6bd7f0dc75bff061d65316cd5ddb5
fana <fanabezabih@gmail.com>
2025-07-01 15:49:40 +0300

    Edited products, api files and fixed errors

--- backend/subscription/models.py
+++ backend/subscription/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from users.models import User
-from products.models import Product
+from product.models import Product
 
 FREQUENCY_CHOICES = [
     ('weekly', 'Weekly'),
